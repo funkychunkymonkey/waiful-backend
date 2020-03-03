2d1a15720fb7f0958d9d30af1a715994f3180e51
Sarah Ting (macbook) <sarah.j.ting@gmail.com>
2020-03-03 17:40:34 +0900

    add data and seeds for exercises

--- db/migrate/20200303013634_create_muscles.rb
+++ db/migrate/20200303013634_create_muscles.rb
@@ -2,8 +2,6 @@ class CreateMuscles < ActiveRecord::Migration[6.0]
   def change
     create_table :muscles do |t|
       t.string :name
-
-      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -61,8 +61,6 @@ ActiveRecord::Schema.define(version: 2020_03_03_053750) do
 
   create_table "muscles", force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "runs", force: :cascade do |t|
