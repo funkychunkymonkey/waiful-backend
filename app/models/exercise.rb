d1b07affcc1b9fe8defcabbf4e7b49f7079e2a6d
Sarah Ting (macbook) <sarah.j.ting@gmail.com>
2020-03-15 22:02:59 +0900

    remove user association from exercises

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -2,5 +2,4 @@ class Exercise < ApplicationRecord
   has_many :exercise_images
   has_and_belongs_to_many :muscles, through: :exercises_muscles
   has_and_belongs_to_many :equipments, through: :equipments_exercises 
-  has_many :user
 end
